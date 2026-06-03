bb4a8a6a9ec553f41b14e133114e0d39ef306bcb
Belka-letyaga <alexsten1811@gmail.com>
2026-06-03 10:42:14 +0500

    null id

--- internal/repository/addprod.go
+++ internal/repository/addprod.go
@@ -6,26 +6,24 @@ import (
 )
 
 func Addproduct(product models.Product) error {
-	var nextID int
-	query := `
-    SELECT COALESCE(
-        (SELECT t1.id + 1 
-         FROM users t1 
-         LEFT JOIN users t2 ON t1.id + 1 = t2.id 
+
+	productQuery := `
+    INSERT INTO products (id, name, description, img_url, category_id)
+    VALUES (
+        COALESCE(
+           
+            (SELECT t1.id + 1 
+             FROM products t1 
+         LEFT JOIN products t2 ON t1.id + 1 = t2.id 
          WHERE t2.id IS NULL 
          ORDER BY t1.id 
-         LIMIT 1), 
+         LIMIT 1),
+        
         1
-    );
-`
-
-	err := db.QueryRow(query).Scan(&nextID)
-	productQuery := `
-        INSERT INTO products (id, name, description, img_url, category_id) 
-        VALUES ($1, $2, $3, $4, $5) 
-        `
-
-	_, err = db.Exec(productQuery, nextID, product.Name, product.Description, product.ImgURL, product.Category_id)
+    ),
+    $1, $2, $3, $4
+);  `
+	_, err := db.Exec(productQuery, product.Name, product.Description, product.ImgURL, product.Category_id)
 	if err != nil {
 		return fmt.Errorf("insert into products failed: %w", err)
 	}
