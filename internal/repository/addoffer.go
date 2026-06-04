e08d190325e1ff29ea23b5ccb4e623336b0609f7
Belka-letyaga <alexsten1811@gmail.com>
2026-06-04 15:59:14 +0500

    g

--- internal/repository/addoffer.go
+++ internal/repository/addoffer.go
@@ -11,7 +11,14 @@ func AddOffer(id, count int, price float64, user_id int) error {
 	productQuery = `UPDATE products 
 			SET offer = true
 			WHERE id = $1;`
+
 	_, err = db.Exec(productQuery, id)
+	productQuery = `
+        UPDATE products 
+        SET min_price = $2  
+        WHERE id = $1 
+          AND (min_price > $2 OR min_price = 0.00);`
+	_, err = db.Exec(productQuery, id, price)
 	if err != nil {
 		log.Println("Ошибка сохранения уведомления в БД:", err)
 		return err
--- internal/repository/updateoffer.go
+++ internal/repository/updateoffer.go
@@ -10,6 +10,12 @@ func UpdateOffer(id int, price float64, count int, user_id int) error {
 			WHERE product_id = $3 AND seller_id=$4;`
 
 	_, err := db.Exec(productQuery, price, count, id, user_id)
+	productQuery = `
+        UPDATE products 
+        SET min_price = $2  
+        WHERE id = $1 
+          AND (min_price > $2 OR min_price = 0.00);`
+	_, err = db.Exec(productQuery, id, price)
 	if err != nil {
 		return fmt.Errorf("failed to update products table: %w", err)
 	}
